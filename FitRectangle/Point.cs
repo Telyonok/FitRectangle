a49462d6c942a0afbaa985431e7ad728f1a24b9e
Ilya_Chvilyov <life270317@gmail.com>
2024-06-12 13:35:54 +0300

    Defined core classes.

---
+++ FitRectangle/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace FitRectangle
+{
+    public class Point
+    {
+        public Point(double x, double y)
+        {
+            X = x; Y = y;
+        }
+
+        public double X {  get; set; }
+        public double Y { get; set; }
+    }
+}
